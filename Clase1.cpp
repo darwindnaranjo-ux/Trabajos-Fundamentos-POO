f172d9d3bd3eb2d28a37bb994f24616d1604d14b
Darwin Naranjo <darwindnaranjo@gmail.com>
2026-05-11 05:14:10 -0500

    Commit Primera clase

---
+++ Clase1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class carro{
+
+    public:
+    int velocidad;
+    void acelerar(){
+        velocidad=velociad+10;
+        std::cout<<"velocidad: " << velocidad;
+    }
+};
+
+int main(){
+
+    
+}
